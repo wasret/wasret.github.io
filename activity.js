b40a7199e5a282a7d653490f68044cc73b2eb657
Ryan Clark <wasret@Ryans-MacBook-Pro.local>
2016-07-15 14:47:24 -0400

    made some changes

--- activity.js
+++ activity.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
  $("#bed").fadeOut();
 
  setTimeout(function(){
-    $("#bed").text("NOW THE MOTHERFUCKERS DEAD");
+    $("#bed").text("NOW THE MOTHER FUCKERS DEAD");
  }, 10000);
  
  $("#bed").fadeIn();
