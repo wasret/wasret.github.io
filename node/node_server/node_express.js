580b869f0c84e253c8ed0fc2af504f95e51139b7
Ryan Clark <wasret@Ryans-MacBook-Pro.local>
2016-07-18 14:51:16 -0400

    worked with node

--- node/node_server/node_express.js
+++ node/node_server/node_express.js
@@ -2,11 +2,15 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World im here!');
 });
 
 app.get('/goodbye', function (req, res) {
   res.send('Goodbye World!');
+});
+
+app.get('/yellow', function (req, res) {
+  res.send('yellow sounds like hello!');
 });
 
 app.listen(3000, function () {
